e8b2641bd90209a036bd0bc3c4d89301eaa5b77f
twd1019624016 <twd1019624016@sina.com>
2018-04-13 10:00:28 +0800

    Update Demo1.java

--- src/main/java/path/Demo1.java
+++ src/main/java/path/Demo1.java
@@ -14,6 +14,7 @@ public class Demo1 {
 	    for (FileStore fileStore : fileStores) {
 			System.out.println(fileStore);
 			System.out.println(fileStore.getTotalSpace());
+		    ///////////////////////////////////
 		}
 	}
 }
